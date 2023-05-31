654764919728f6e0aa372ef73fad7f69b5870809
Ilya Petrov <ipb.tech@yandex.ru>
2023-05-31 17:28:08 +0300

    Processing GetDataButton and fill datagrid

--- ConfluenceTableParser/GUI_WinForms/ConfMaterialGUI.Designer.cs
+++ ConfluenceTableParser/GUI_WinForms/ConfMaterialGUI.Designer.cs
@@ -32,7 +32,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.GetDataButton = new System.Windows.Forms.Button();
             this.JSONExportButton = new System.Windows.Forms.Button();
-            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.DataGridMaterialView = new System.Windows.Forms.DataGridView();
             this.IDColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.MaterialGroupColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.MaterialTitleColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -40,7 +40,7 @@
             this.MaterialNotationColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.MaterialMeasureUnitsColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.MaterialUsageColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridMaterialView)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -60,7 +60,7 @@
             this.GetDataButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.GetDataButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.GetDataButton.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.GetDataButton.Location = new System.Drawing.Point(742, 74);
+            this.GetDataButton.Location = new System.Drawing.Point(750, 74);
             this.GetDataButton.Name = "GetDataButton";
             this.GetDataButton.Size = new System.Drawing.Size(110, 30);
             this.GetDataButton.TabIndex = 2;
@@ -75,7 +75,7 @@
             this.JSONExportButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.JSONExportButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.JSONExportButton.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.JSONExportButton.Location = new System.Drawing.Point(742, 142);
+            this.JSONExportButton.Location = new System.Drawing.Point(750, 142);
             this.JSONExportButton.Name = "JSONExportButton";
             this.JSONExportButton.Size = new System.Drawing.Size(110, 30);
             this.JSONExportButton.TabIndex = 3;
@@ -83,15 +83,19 @@
             this.JSONExportButton.UseVisualStyleBackColor = false;
             this.JSONExportButton.Click += new System.EventHandler(this.JSONExportButton_Click);
             // 
-            // DataGridView
+            // DataGridMaterialView
             // 
-            this.DataGridView.AllowUserToAddRows = false;
-            this.DataGridView.AllowUserToDeleteRows = false;
-            this.DataGridView.AllowUserToResizeRows = false;
-            this.DataGridView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.DataGridView.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
-            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DataGridMaterialView.AllowUserToAddRows = false;
+            this.DataGridMaterialView.AllowUserToDeleteRows = false;
+            this.DataGridMaterialView.AllowUserToResizeRows = false;
+            this.DataGridMaterialView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DataGridMaterialView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DataGridMaterialView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.DataGridMaterialView.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.DataGridMaterialView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridMaterialView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.IDColumn,
             this.MaterialGroupColumn,
             this.MaterialTitleColumn,
@@ -99,82 +103,73 @@
             this.MaterialNotationColumn,
             this.MaterialMeasureUnitsColumn,
             this.MaterialUsageColumn});
-            this.DataGridView.GridColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.DataGridView.Location = new System.Drawing.Point(17, 40);
-            this.DataGridView.Margin = new System.Windows.Forms.Padding(2);
-            this.DataGridView.Name = "DataGridView";
-            this.DataGridView.RowHeadersVisible = false;
-            this.DataGridView.RowHeadersWidth = 20;
-            this.DataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
-            this.DataGridView.RowTemplate.Height = 33;
-            this.DataGridView.Size = new System.Drawing.Size(700, 440);
-            this.DataGridView.TabIndex = 4;
+            this.DataGridMaterialView.GridColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.DataGridMaterialView.Location = new System.Drawing.Point(17, 40);
+            this.DataGridMaterialView.Margin = new System.Windows.Forms.Padding(2);
+            this.DataGridMaterialView.Name = "DataGridMaterialView";
+            this.DataGridMaterialView.RowHeadersVisible = false;
+            this.DataGridMaterialView.RowHeadersWidth = 20;
+            this.DataGridMaterialView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.DataGridMaterialView.RowTemplate.Height = 33;
+            this.DataGridMaterialView.RowTemplate.ReadOnly = true;
+            this.DataGridMaterialView.Size = new System.Drawing.Size(708, 503);
+            this.DataGridMaterialView.TabIndex = 4;
             // 
             // IDColumn
             // 
             this.IDColumn.HeaderText = "ID";
             this.IDColumn.MinimumWidth = 10;
             this.IDColumn.Name = "IDColumn";
-            this.IDColumn.Width = 40;
             // 
             // MaterialGroupColumn
             // 
             this.MaterialGroupColumn.HeaderText = "Group";
             this.MaterialGroupColumn.MinimumWidth = 10;
             this.MaterialGroupColumn.Name = "MaterialGroupColumn";
-            this.MaterialGroupColumn.Width = 100;
             // 
             // MaterialTitleColumn
             // 
             this.MaterialTitleColumn.HeaderText = "Title";
             this.MaterialTitleColumn.MinimumWidth = 10;
             this.MaterialTitleColumn.Name = "MaterialTitleColumn";
-            this.MaterialTitleColumn.Width = 140;
             // 
             // MaterialNameColumn
             // 
             this.MaterialNameColumn.HeaderText = "Name";
             this.MaterialNameColumn.MinimumWidth = 10;
             this.MaterialNameColumn.Name = "MaterialNameColumn";
-            this.MaterialNameColumn.Width = 140;
             // 
             // MaterialNotationColumn
             // 
             this.MaterialNotationColumn.HeaderText = "Notation";
             this.MaterialNotationColumn.MinimumWidth = 10;
             this.MaterialNotationColumn.Name = "MaterialNotationColumn";
-            this.MaterialNotationColumn.Width = 140;
             // 
             // MaterialMeasureUnitsColumn
             // 
             this.MaterialMeasureUnitsColumn.HeaderText = "Units";
             this.MaterialMeasureUnitsColumn.MinimumWidth = 10;
             this.MaterialMeasureUnitsColumn.Name = "MaterialMeasureUnitsColumn";
-            this.MaterialMeasureUnitsColumn.Width = 40;
             // 
             // MaterialUsageColumn
             // 
             this.MaterialUsageColumn.HeaderText = "Usage";
             this.MaterialUsageColumn.MinimumWidth = 10;
             this.MaterialUsageColumn.Name = "MaterialUsageColumn";
-            this.MaterialUsageColumn.Width = 100;
             // 
             // ConfMaterialGUI
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(876, 498);
-            this.Controls.Add(this.DataGridView);
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.DataGridMaterialView);
             this.Controls.Add(this.JSONExportButton);
             this.Controls.Add(this.GetDataButton);
             this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(400, 200);
             this.Name = "ConfMaterialGUI";
             this.Text = "ConfluenceUsageMaterialData";
-            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridMaterialView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -185,7 +180,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button GetDataButton;
         private System.Windows.Forms.Button JSONExportButton;
-        private System.Windows.Forms.DataGridView DataGridView;
+        private System.Windows.Forms.DataGridView DataGridMaterialView;
         private System.Windows.Forms.DataGridViewTextBoxColumn IDColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn MaterialGroupColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn MaterialTitleColumn;
